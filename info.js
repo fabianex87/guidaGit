54bce1845082a590df87ffd23ed1c300ed81efe6
fabian oscco <fabian587hotmail.com>
2020-04-09 12:23:30 +0200

    iniziamo con git

---
+++ info.js
@@ -0,0 +1,17 @@
+/**
+ * tre stati
+ * working directory (dove lavori)
+ * git add
+ * staging area
+ * git commit
+ * repository
+ * 
+ * git init: per inizializzare con git
+ * git add: per aggiungere
+ * git status: per vedere in quale di quei tre stati stà il tuo git
+ * 
+ * git push: 
+ * git pull:
+ * git clone: fai una copia
+ * 
+ */
