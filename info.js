bbce9131012384ef15c2e3af406b8000cf0465e1
fabianex87 <fabian587@hotmail.com>
2020-05-23 11:29:03 +0200

    Add definition the git

--- info.js
+++ info.js
@@ -1,17 +1,24 @@
 /**
  * tre stati
- * working directory (dove lavori)
+ * 1) working directory (dove lavori)
  * git add
- * staging area
+ * 2) staging area
  * git commit
- * repository
- * 
+ * 3) git directory (repository)
+ * 1  -------->  2  -------->  3  ------> 1
+ *    (git add)    (git commit)   (checkout)
+ *
  * git init: per inizializzare con git
  * git add: per aggiungere
  * git status: per vedere in quale di quei tre stati stà il tuo git
- * 
- * git push: 
+ *
+ * git push:
  * git pull:
  * git clone: fai una copia
- * 
+ *
+ * git add.: mette tutto in staging area
+ * git commit: fa una specie di foto e lo mette nell'header
+ * git log: lista di tutti i commit
+ *
+ *
  */
