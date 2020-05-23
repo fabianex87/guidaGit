c913464ce650c2b531dd5819fa63cb1709cf4591
fabianex87 <fabian587@hotmail.com>
2020-05-23 18:53:36 +0200

    add information info.js

--- info.js
+++ info.js
@@ -1,4 +1,9 @@
 /**
+ * configurazione di git
+ * $ git config --global user.name "nome"
+ * git config --global user.email email@example.com
+ * git config --list: ti appare tutta la configurazione
+ *
  * tre stati
  * 1) working directory (dove lavori)
  * git add
@@ -16,7 +21,8 @@
  * git pull:
  * git clone: fai una copia
  *
- * git add.: mette tutto in staging area
+ * git add .: mette tutto in staging area
+ * git add -A: simile a git add . ma in questo caso mette tutto anche al commit
  * git commit: fa una specie di foto e lo mette nell'header
  * git log: lista di tutti i commit
  * git checkout bbce9131012384ef15c2e3af406b8000cf0465e1: ti permette di andare
@@ -45,4 +51,53 @@
  * repository:
  * local: il tuo pc
  * remote: github o gitlab
+ *
+ * prima di fare un push, fare sempre il pull
+ * prima di lavorare, fai il pull e poi il push
+ *
+ * git rebase: ti fa risparmiare diversi passi
+ *
+ * fork: quando fai clic in fork (github) il repository te lo copi nel tuo repository
+ *
+ * problemi quando lavori su un branch diverso
+ * git reset HEAD^1: eliminare l'ultimo commit, ritorna al commit precedente
+ * poi git add .
+ * poi lo metti in uno stash: git stash --save "messaggio" o solamente git stash
+ * creo il branch (dovevo lavorare su questo branch)
+ * git checkout -b branchLavoro
+ * prendo quello che ce nello stash
+ * git stash pop
+ * git add .
+ * git commit -m "Modifica nuova."
+ * git push ma non esiste quel branch, allora non possiamo fare un pull ma solo push:
+ * git push -u origin branchLavoro: -u perché non esiste quel branch ed è il primo commit
+ * e al fare un pull Request da fork(hai copiato un repository di un altro), quel pull Request
+ * per default andra' a quel autore originario, quindi devi cambiare e fare il PR alla persona indicata (github)
+ *
+ * ho fatto il lavoro su featureBranch e voglio metterlo in develop cioè fare il merge su develop
+ * merge di featureBranch su develop ma non con il metodo della pagina
+ * ma in questo modo:
+ * merge in locale
+ * mi sposto su develop
+ * git merge featureBranch (copio quello di featureBranch in develop)
+ * git push -u origin develop
+ *
+ * di solito origin si fa solo la prima volta, poi si aggancia da solo
+ * ti puo dare errore se non fai il pull
+ * quando lavori su un branch diverse persone, DEVI FARE IL PULL perché se nel
+ * remoto ci sono dei cambi dovuto a che qualche persona ha fatto qualche modifica
+ * e tu vuoi fare il push, ti daraà problemi perché nel tuo pc (locale) non ce quello che ce nel remoto
+ * a prescindere se fai il pull ti darà problemi di conflitto perché bisogna
+ * decidere con quale modifica deve rimanere
+ * git pull origin develop (mi porto i cambi)
+ * appaiono i conflitti
+ * apri VSCode, e scegli con quello che vuoi rimanere "use" o ce un commando che accetta tutto, Accept current change
+ * poi git status
+ * git add .
+ * git commit -m "Risolve conflitto."
+ * git push origin develop -u
+ *
+ * permessi ad altri utenti in github
+ * setting/manage acces/ inviate a collaborator/nome usser o email
+ *
  */
