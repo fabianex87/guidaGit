b3dfa5d94571a6457dfdb9c472568390973bfec2
fabianex87 <fabian587@hotmail.com>
2020-07-31 23:39:54 +0200

    Modify file comandiDaEseguire.js

--- comandiDaEseguire.js
+++ comandiDaEseguire.js
@@ -2,6 +2,9 @@
  * se stai nel push di origine
  * git push
  *
+ * altrimenti
+ * git push origin nameBranch
+ *
  * se hai creato un branch figlio
  * git push <remote>, cioè
  * git push https://github.com/fabianex87/guidaGit.git
