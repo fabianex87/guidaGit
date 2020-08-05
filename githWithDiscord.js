e17d8ebf900b5c33dd5dfc7c12f9292539655c02
fabianex87 <fabian587@hotmail.com>
2020-08-05 22:10:57 +0200

    Add file for github with discor.

---
+++ githWithDiscord.js
@@ -0,0 +1,16 @@
+/**
+ * collegare le notifiche di git con discord
+ *
+ * lato Discord
+ * crei un canale in discord
+ * poi vai a impostazioni/integrations/create Webhook
+ * e copy il link del webhook che hai appena creato
+ *
+ * lato Github
+ * Settings/Webhook/Add Webhook
+ * incolli il link di webhook di discord
+ * https://discordapp.com/api/webhooks/740660338407637152/M8208rWwJwlrq2Vlh4OdG-jDFWhZv4-jQ_OogHhekWZupzVlVL3sPGu6HWg7oOZ5YOT4
+ * e aggiungi il canale, quindi
+ * https://discordapp.com/api/webhooks/740660338407637152/M8208rWwJwlrq2Vlh4OdG-jDFWhZv4-jQ_OogHhekWZupzVlVL3sPGu6HWg7oOZ5YOT4/github
+ *
+ */
