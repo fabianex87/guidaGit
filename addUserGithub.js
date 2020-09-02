3116da64406994ee3376452d720113bb544a19c7
fabianex87 <37955119+fabianex87@users.noreply.github.com>
2020-09-02 20:56:36 +0200

    Revert "Feature/fab"

--- addUserGithub.js
+++
@@ -1,5 +0,0 @@
-/**
- * permessi ad altri utenti in github per collaborare con il progetto
- * Settings/manage acces/ inviate a collaborator/nome user o email
- *
- */
--- proteggereBranchMaster.js
+++
@@ -1,8 +0,0 @@
-/**
- * Su github
- * Setting/Branches/Add rule/master(digitare su branch name pattern)
- *
- * Protect matching branches> Require pull request reviews before merging
- * (cioè leggere il codice prima di fare il merge)
- *
- */
